facc7fc3fdb8822d0a85bb7ce9f15c6066a07e04
athzh1998 <ath.zh1998@gmail.com>
2021-09-27 16:16:26 +0300

    install vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <b-container>
+    <b-row align-v="center">
+      <b-col>
+        <b-card
+          title="This is a Card"
+          tag="article"
+          style="max-width: 10rem"
+          class="mb-2"
+        >
+          <b-card-text>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Consequuntur, cum maiores minus ab, ipsum voluptas harum autem
+            fugiat porro repellendus quas iste vero officiis officia labore
+            fugit iure. Voluptates, quidem.
+          </b-card-text>
+          <b-button variant="primary"> OK </b-button>
+        </b-card>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
-  },
+  components: {},
 };
 </script>
