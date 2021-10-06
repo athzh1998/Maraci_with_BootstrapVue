ac2474480df5bf23a472dc42bbff76d8425dfd48
athzh1998 <ath.zh1998@gmail.com>
2021-10-06 12:25:11 +0300

    page changes when user login

--- src/components/TopHeader.vue
+++ src/components/TopHeader.vue
@@ -18,9 +18,17 @@
           <b-nav-item to="/">الرئيسية</b-nav-item>
           <b-nav-item to="/about">من نحن</b-nav-item>
           <b-nav-item to="/blogs">المدونة</b-nav-item>
-          <b-button @click="goToLogin()" size="lg" class="my-3 my-sm-0"
-            >تسجيل الدخول</b-button
-          >
+          <div v-if="loggedIn">
+            <b-dropdown id="dropdown-left" text="Left align" variant="light" class="m-2">
+              <b-dropdown-item href="#">Action</b-dropdown-item>
+              <b-dropdown-item href="#">Another action</b-dropdown-item>
+              <b-dropdown-item href="#">Something else here</b-dropdown-item>
+            </b-dropdown>
+          </div>
+          <div v-else>
+            <b-button @click="goToLogin()" size="lg" class="my-3 my-sm-0"
+            >تسجيل الدخول</b-button>
+          </div>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
@@ -34,6 +42,11 @@ export default {
       console.log("I am in the go to login method");
       this.$router.push('/login'); 
     }
+  },
+  data: function(){
+    return {
+    loggedIn: true
+    };
   }
 };
 </script>
--- src/components/cover.vue
+++ src/components/cover.vue
@@ -3,13 +3,26 @@
         <b-col md="6" class="p-5">
             <div>
                 <p class="cover-paraghraph">هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى، حيث يمكنك أن تولد مثل هذا النص أو العديد من النصوص الأخرى إضافة إلى زيادة عدد الحروف التى يولدها التطبيق.</p>
-                <b-button size="lg" type="submit" class="w-75">سجل الآن</b-button>
+                <div v-if="loggedIn">
+                    <b-button size="lg" type="submit" class="w-75">انشر رحلتك 
+                        <b-icon icon="plus" font-scale="1.5" aria-hidden="true"></b-icon>
+                    </b-button>
+                </div>
+                <div v-else>
+                    <b-button size="lg" type="submit" class="w-75">سجل الآن</b-button>
+                </div>
             </div>
         </b-col>
     </b-row>
 </template>
 <script>
 export default {
+    name:"cover",
+    data: function(){
+    return {
+    loggedIn: true
+    };
+  }
 }
 </script>
 <style>
